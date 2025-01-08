e832705573a52474f5e544c5bb8a118ab5740294
Niklas Zeegers <63072839+Dark-Pirate69@users.noreply.github.com>
2025-01-08 20:42:54 +0100

    Course-controller first test

---
+++ server/src/controllers/course-controller.js
@@ -0,0 +1,46 @@
+const Course = require("../models/course-model");
+
+exports.createCourse = async (req, res, next) => {
+    const title = req.body.title;
+    try {
+        const newCourse = await new Course({name}).save();
+        return res.status(201).json({
+            message: `New course created!`,
+            data: newCourse,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+exports.getAllCourses = async (_, res, next) => {
+    try {
+        const foundCourses = await Course.find({});
+        return res.status(200).json({
+            message: `Found all courses`,
+            data: foundCourses,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+exports.deleteCourse = async (req, res, next) => {
+    const courseId = req.params.todoId;
+    try {
+        const deletedCourse = await Course.findByIdAndDelete(courseId);
+
+        if (!deletedCourse) {
+            const error = new Error(`Course not found with id: ${courseId}`);
+            error.status = 404;
+            throw error;
+        }
+
+        return res.status(200).json({
+            message: `Course with successfully deleted.`,
+            data: deletedCourse,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
