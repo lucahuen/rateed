09b8eade6efde67d9b8ac1a9ed9b8f07304d0b5c
lucahuen <l.huening@outlook.com>
2025-01-09 19:31:06 +0100

    renamed user-service.js

--- react-client/src/context/api-context.jsx
+++ react-client/src/context/api-context.jsx
@@ -1,14 +1,14 @@
 import React, {createContext} from "react";
 import TodoService from "../services/todo-service";
 import CourseService from "../services/course-service";
-import RegisterService from "../services/register-service.js";
+import UserService from "../services/user-service.js";
 
 const ApiContext = createContext(null);
 
 const ApiProvider = ({children}) => {
     const todoService = new TodoService();
     const courseService = new CourseService();
-    const registerService = new RegisterService()
+    const registerService = new UserService()
 
     return (
         <ApiContext.Provider value={{
--- react-client/src/services/user-service.js
+++ react-client/src/services/user-service.js
@@ -1,9 +1,9 @@
 import api from "../utils/backend.js";
 
-export default class RegisterService{
+export default class UserService {
     requestRegister = async (username, password) => {
         try{
-            const response = await api.post("/users/create", {username, password});
+            const response = await api.post("/api/users/create", {username, password});
             return response.data;
         }catch (error){
             throw error.response.data.error.message
