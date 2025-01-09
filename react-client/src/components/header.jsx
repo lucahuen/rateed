c5ad11d6ef6a9d88f9054ed7b9e14bb9ee7917fe
lucahuen <l.huening@outlook.com>
2025-01-09 21:46:05 +0100

    logout working

--- react-client/src/components/header.jsx
+++ react-client/src/components/header.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import {AppBar, Box, Button, ButtonBase, Toolbar, Typography} from "@mui/material";
 import {useNavigate} from "react-router-dom";
 import PropTypes from "prop-types";
+import Cookies from "js-cookie";
 
 const Header = ({siteInformation}) => {
     const navigate = useNavigate()
+    const sessionId = Cookies.get("auth")
     const handleLogin = () => {
         navigate("/login");
+    }
+    const handleLogout = () => {
+        Cookies.remove("auth")
+        window.location.reload()
     }
 
     const handleHomepage = () => {
@@ -41,21 +47,39 @@ const Header = ({siteInformation}) => {
 
                 {/* LoginButton-Button rechts */}
                 <Box sx={{marginLeft: "auto"}}>
-                    <Button
-                        variant="contained"
-                        onClick={handleLogin}
-                        sx={{
-                            backgroundColor: 'white',
-                            color: 'black',
-                            fontWeight: 'bold',
-                            '&:hover': {
-                                backgroundColor: '#f5f5f5', // leicht grauer Hover-Effekt
-                            },
-                            border: '1px solid black' // optional, um den Button hervorzuheben
-                        }}
-                    >
-                        Login
-                    </Button>
+                    {sessionId ? (
+                        <Button
+                            variant="contained"
+                            onClick={handleLogout}
+                            sx={{
+                                backgroundColor: 'white',
+                                color: 'black',
+                                fontWeight: 'bold',
+                                '&:hover': {
+                                    backgroundColor: '#f5f5f5', // leicht grauer Hover-Effekt
+                                },
+                                border: '1px solid black' // optional, um den Button hervorzuheben
+                            }}
+                        >
+                            Logout
+                        </Button>
+                    ) : (
+                        <Button
+                            variant="contained"
+                            onClick={handleLogin}
+                            sx={{
+                                backgroundColor: 'white',
+                                color: 'black',
+                                fontWeight: 'bold',
+                                '&:hover': {
+                                    backgroundColor: '#f5f5f5', // leicht grauer Hover-Effekt
+                                },
+                                border: '1px solid black' // optional, um den Button hervorzuheben
+                            }}
+                        >
+                            Login
+                        </Button>
+                    )}
                 </Box>
             </Toolbar>
         </AppBar>
