de84b1c37e167181785d1f2cee3eaa5611b2d80f
lucahuen <l.huening@outlook.com>
2025-01-09 20:02:11 +0100

    todo-routes.js rollbacked

--- server/src/routes/todo-routes.js
+++ server/src/routes/todo-routes.js
@@ -4,10 +4,10 @@ const requestLogger = require("../middleware/logger-middleware");
 
 const todoRouter = express.Router();
 
-todoRouter.post("/todos/create", requestLogger, todoController.createTodo);
+todoRouter.post("/create", requestLogger, todoController.createTodo);
 
-todoRouter.get("/todos", requestLogger, todoController.getAllTodos);
+todoRouter.get("/", requestLogger, todoController.getAllTodos);
 
-todoRouter.delete("/todos/:todoId", requestLogger, todoController.deleteTodo);
+todoRouter.delete("/:todoId", requestLogger, todoController.deleteTodo);
 
 module.exports = todoRouter;
