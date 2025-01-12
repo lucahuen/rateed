cd07104d37f830d7c6afc4da26ccc7263ba9c1c8
Samed <samed.coepuer@stud.uni-due.de>
2025-01-12 18:13:34 +0100

    Course modul score + counter

--- server/src/models/course-model.js
+++ server/src/models/course-model.js
@@ -6,6 +6,8 @@ const courseSchema = new Schema({
     name: { type: String, required: true },
     semester: { type: String, required: true },
     createdAt: { type: Date, default: Date.now() },
+    score: {type: int, default: 0},
+    counter: {type: int, default: 0},
 });
 
 module.exports = mongoose.model("Course", courseSchema);
