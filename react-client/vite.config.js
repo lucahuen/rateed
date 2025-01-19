f34e17ad8a61e67fcb364687d7b0adcc3bbcb1fd
lucahuen <l.huening@outlook.com>
2025-01-19 19:18:36 +0100

    reverted vite.config.js

--- react-client/vite.config.js
+++ react-client/vite.config.js
@@ -1,18 +1,26 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
+//
+// export default defineConfig({
+//     server: {
+//         port: 5173,
+//         proxy: {
+//             '/api': {
+//                 target: 'http://localhost:3000',
+//                 changeOrigin: true,
+//                 rewrite: (path) => path.replace(/^\/api/, ''), // Entferne '/api'
+//             },
+//         },
+//         watch: {
+//             usePolling: true,
+//         },
+//     },
+//     plugins: [react()],
+// });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-        proxy: {
-            '/api': {
-                target: 'https://rateed.onrender.com:10000',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ''), // Entferne '/api'
-            },
-        },
-        watch: {
-            usePolling: true,
-        },
-    },
     plugins: [react()],
-});
+})
