a7b5d85a208906b3f1a9cb88a261ce3441c5fd68
lucahuen <l.huening@outlook.com>
2025-01-19 19:14:10 +0100

    what if

--- react-client/vite.config.js
+++ react-client/vite.config.js
@@ -1,19 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import {defineConfig} from 'vite';
+// import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
     server: {
-        port: 5173,
-        proxy: {
-            '/api': {
-                target: 'http://localhost:3000',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ''), // Entferne '/api'
-            },
-        },
-        watch: {
-            usePolling: true,
-        },
-    },
-    plugins: [react()],
-});
+        //     port: 5173,
+        //     proxy: {
+        //         '/api': {
+        //             target: 'http://localhost:3000',
+        //             changeOrigin: true,
+        //             rewrite: (path) => path.replace(/^\/api/, ''), // Entferne '/api'
+        //         },
+        //     },
+        //     watch: {
+        //         usePolling: true,
+        //     },
+        // },
+        // plugins: [react()],
+// });
+    }
+})
