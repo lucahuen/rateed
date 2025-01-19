0bf515e3c46e6e3f7102c0fbd5b29dc30d1dd73c
lucahuen <l.huening@outlook.com>
2025-01-19 19:21:38 +0100

    maybe ts works

--- react-client/vite.config.js
+++ react-client/vite.config.js
@@ -22,5 +22,12 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+        proxy: {
+            '/api': {
+                rewrite: (path) => path.replace(/^\/api/, ''), // Entferne '/api'
+            },
+        }
+    },
     plugins: [react()],
 })
